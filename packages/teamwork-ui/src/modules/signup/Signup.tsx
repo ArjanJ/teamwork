d30b8d208407807dfae1f4ab8535f166e3b42fec
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-04-15 12:00:53 -0700

    create useForm custom hook

--- packages/teamwork-ui/src/modules/signup/Signup.tsx
+++ packages/teamwork-ui/src/modules/signup/Signup.tsx
@@ -2,6 +2,7 @@ import { navigate } from '@reach/router';
 import React, { SFC, useState } from 'react';
 
 import { auth, firebase } from '../../firebase';
+import { useForm } from '../../hooks/useForm';
 
 interface SignupProps {
   path: string;
@@ -19,12 +20,54 @@ export const Signup: SFC<SignupProps> = () => {
     }
   };
 
+  const onSubmit = () => {
+    console.log(values);
+  };
+
+  const { handleInputChange, handleSubmit, values } = useForm({
+    initialValues: { email: '', password: '', confirmPassword: '' },
+    onSubmit,
+  });
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Signup</h1>
       <button onClick={signupWithGoogle} type="button">
         Register with Google
       </button>
+      <div>
+        <label>Email</label>
+        <input
+          id="email"
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          value={values.email}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          id="password"
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          value={values.password}
+        />
+      </div>
+      <div>
+        <label>Confirm Password</label>
+        <input
+          id="confirmPassword"
+          name="confirmPassword"
+          onChange={handleInputChange}
+          type="password"
+          value={values.confirmPassword}
+        />
+      </div>
+      <div>
+        <button type="submit">Create account</button>
+      </div>
     </form>
   );
 };
