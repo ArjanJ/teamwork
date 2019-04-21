efecec0d01f1c4c0fe39d3efba55d822b1b36d8d
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-04-20 20:21:36 -0700

    fixed tsconfig (default firebase one is trash, copied the CRA one)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as firebaseHelper from 'firebase-functions-helper';
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
+import functions from 'firebase-functions';
+import admin from 'firebase-admin';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
 admin.initializeApp(functions.config().firebase);
 
@@ -19,8 +18,10 @@ main.use(bodyParser.urlencoded({ extended: false }));
 
 export const webApi = functions.https.onRequest(main);
 
-// Add new contact
-app.post('/users', (req, res) => {
-  firebaseHelper.firestore.createNewDocument(db, usersCollection, req.body);
-  res.send('Create a new user');
+app.post('/users', (req: Request, res: Response) => {
+  db.collection(usersCollection)
+    .doc(req.body.email)
+    .set(req.body)
+    .then(() => res.status(200).send({ success: true }))
+    .catch(() => res.status(500).send({ error: true }));
 });
