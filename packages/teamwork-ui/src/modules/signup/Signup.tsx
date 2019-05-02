a1b8ac0a9908ed6e8d5019fd22980e7cc8725eb6
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-05-01 21:14:24 -0700

    more styling signup page

--- packages/teamwork-ui/src/modules/signup/Signup.tsx
+++ packages/teamwork-ui/src/modules/signup/Signup.tsx
@@ -57,18 +57,26 @@ export const Signup: SFC<SignupProps> = () => {
   return (
     <Backdrop>
       <Form onSubmit={handleSubmit}>
-        <Heading>Create your account</Heading>
-        <P>Register with your work Google account</P>
-        <BigButton onClick={signIn} type="button">
-          Register with Google
-        </BigButton>
-        <P>Or, register with your email</P>
+        <Box mb="24px">
+          <Heading>Create your account</Heading>
+          <P>Register with your work Google account</P>
+        </Box>
+        <Box mb="24px">
+          <BigButton onClick={signIn} type="button">
+            <GoogleLogo />
+            <span>Register with Google</span>
+          </BigButton>
+        </Box>
+        <Box mb="36px">
+          <P>Or, register with your email</P>
+        </Box>
         <Box mb="24px">
           <Label>Email</Label>
           <Input
             id="email"
             name="email"
             onChange={handleInputChange}
+            placeholder="Your email"
             required
             type="email"
             value={values.email}
@@ -80,29 +88,31 @@ export const Signup: SFC<SignupProps> = () => {
             id="password"
             name="password"
             onChange={handleInputChange}
+            placeholder="Create password"
             required
             type="password"
             value={values.password}
           />
         </Box>
-        <Box mb="24px">
+        <Box mb="36px">
           <Label>Confirm Password</Label>
           <Input
             id="confirmPassword"
             name="confirmPassword"
             onChange={handleInputChange}
+            placeholder="Confirm password"
             required
             type="password"
             value={values.confirmPassword}
           />
         </Box>
         <Flex justifyContent="center" mb="24px">
-          <button type="submit">Create account</button>
+          <Button type="submit">Create account</Button>
         </Flex>
         <Box>
-          <p>
+          <P>
             Already have an account? <Link to="/signup">Login</Link>
-          </p>
+          </P>
         </Box>
       </Form>
     </Backdrop>
@@ -120,30 +130,53 @@ const Backdrop = styled.div`
 const Heading = styled.h1`
   color: #f8cf83;
   font-size: 30px;
+  margin-bottom: 10px;
 `;
 
 const P = styled.p`
   color: white;
+  font-weight: 500;
 `;
 
 const Form = styled.form`
   max-width: 360px;
+  text-align: center;
   width: 100%;
 `;
 
 const BigButton = styled.button`
-  background: white;
-  border: none;
-  height: 32px;
-  font-weight: 700;
+  align-items: center;
+  background: #f3f3f3;
+  border-radius: 2px;
+  color: #2d77ee;
+  display: flex;
+  font-size: 15px;
+  font-weight: 600;
+  height: 40px;
+  padding: 0 12px;
   width: 100%;
+
+  span {
+    flex: 1;
+  }
 `;
 
 const Input = styled.input`
   background: rgba(255, 255, 255, 0.2);
-  border: none;
-  height: 32px;
+  color: white;
+  height: 48px;
+  padding: 0 16px;
+  transition: all 0.2s ease-out;
   width: 100%;
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.7);
+  }
+
+  &:focus,
+  &:hover {
+    background: rgba(255, 255, 255, 0.3);
+  }
 `;
 
 const Label = styled.label`
@@ -151,5 +184,39 @@ const Label = styled.label`
   height: 0;
   visibility: hidden;
 `;
+
+const Button = styled.button`
+  background: #2d77ee;
+  border-radius: 2px;
+  color: white;
+  font-size: 15px;
+  font-weight: 700;
+  height: 40px;
+  padding: 0 20px;
+`;
+
+const GoogleLogo = () => (
+  <svg
+    height="20px"
+    width="20px"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 48 48"
+  >
+    <defs>
+      <path
+        id="a"
+        d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+      />
+    </defs>
+    <clipPath id="b">
+      <use xlinkHref="#a" overflow="visible" />
+    </clipPath>
+    <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+    <path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" />
+    <path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" />
+    <path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" />
+  </svg>
+);
 
 export default Signup;
