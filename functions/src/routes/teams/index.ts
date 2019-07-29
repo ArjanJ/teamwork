240a6ab98d83e612902ba0e35e23330f71ae73e2
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-07-29 14:04:27 -0700

    get GET teams/:id route working again

--- functions/src/routes/teams/index.ts
+++ functions/src/routes/teams/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import { get } from './get';
 import { post } from './post';
 
 export const teamsRouter = Router();
 
+teamsRouter.get('/:id', get);
 teamsRouter.post('/', post);
