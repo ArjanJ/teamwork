6d96b9e186625e2ab92ead717fffa640e12db2e9
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-07-29 00:08:38 -0700

    start post method for teams but realized something that won't work

--- packages/teamwork-ui/functions/src/routes/teams/post.ts
+++ packages/teamwork-ui/functions/src/routes/teams/post.ts
@@ -1,3 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-export const post = (req: Request, res: Response, next: NextFunction) => {};
+export const post = (req: Request, res: Response, next: NextFunction) => {
+  const { body, decodedToken } = req;
+  const { company, uid } = decodedToken;
+};
