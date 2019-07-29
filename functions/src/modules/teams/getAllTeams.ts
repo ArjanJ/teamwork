9da1cd24f3663c2cc56e1c7147a6fa8aebe80c5d
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-07-28 23:56:06 -0700

    work on teams api models

--- functions/src/modules/teams/getAllTeams.ts
+++ functions/src/modules/teams/getAllTeams.ts
@@ -1,1 +1,11 @@
-export const getAllTeams = () => {};
+import { db } from '../../config/firebase';
+import { Company } from '../companies/types';
+import { TEAMS_COLLECTION } from './constants';
+
+export const GET_ALL_TEAMS = 'GET_ALL_TEAMS';
+
+export const getAllTeams = async (company: Company) =>
+  await db
+    .collection(TEAMS_COLLECTION)
+    .where('company.name', '==', company.name)
+    .get();
