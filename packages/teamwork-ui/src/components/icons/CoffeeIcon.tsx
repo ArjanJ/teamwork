684e382e9ab33f85f15d0d0bcbdfc43745651190
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-07-30 23:47:55 -0700

    move icons to components/icons

---
+++ packages/teamwork-ui/src/components/icons/CoffeeIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const CoffeeIcon = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M8 0H6V3H8V0Z" fill="white" />
+    <path d="M4 0H2V3H4V0Z" fill="white" />
+    <path d="M12 0H10V3H12V0Z" fill="white" />
+    <path
+      d="M15 5H1C0.4 5 0 5.4 0 6V13C0 14.7 1.3 16 3 16H10C11.7 16 13 14.7 13 13V11C14.7 11 16 9.7 16 8V6C16 5.4 15.6 5 15 5ZM14 8C14 8.6 13.6 9 13 9V7H14V8Z"
+      fill="white"
+    />
+  </svg>
+);
--- packages/teamwork-ui/src/modules/teams/components/TeamMembers.tsx
+++ packages/teamwork-ui/src/modules/teams/components/TeamMembers.tsx
@@ -9,6 +9,7 @@ import {
   Team,
   TeamMember,
 } from '../../../../functions/src/modules/teams/types';
+import { MenuIcon } from '../../../components/icons/MenuIcon';
 import { Toggle } from '../../../components/toggle/Toggle';
 import { Color } from '../../../styles/Color';
 import { Easing } from '../../../styles/Easing';
@@ -101,34 +102,6 @@ export const TeamMembers: FunctionComponent<TeamMembersProps> = ({ team }) => {
     </Box>
   );
 };
-
-const MenuIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
-    x="0px"
-    y="0px"
-    width="16px"
-    height="16px"
-    viewBox="0 0 16 16"
-  >
-    <g transform="translate(0, 0)">
-      <path
-        fill={Color.NAVY}
-        d="M8,14H1c-0.552,0-1-0.448-1-1v0c0-0.552,0.448-1,1-1h7c0.552,0,1,0.448,1,1v0C9,13.552,8.552,14,8,14z"
-      />{' '}
-      <path
-        fill={Color.NAVY}
-        d="M15,4H1C0.448,4,0,3.552,0,3v0c0-0.552,0.448-1,1-1h14c0.552,0,1,0.448,1,1v0C16,3.552,15.552,4,15,4z"
-      />{' '}
-      <path
-        data-color="color-2"
-        fill={Color.NAVY}
-        d="M15,9H1C0.448,9,0,8.552,0,8v0c0-0.552,0.448-1,1-1h14c0.552,0,1,0.448,1,1v0 C16,8.552,15.552,9,15,9z"
-      />
-    </g>
-  </svg>
-);
 
 const Subheading = styled.h2`
   color: white;
