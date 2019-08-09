bd18a241ac5d4281099e72acb3322ca8c45945a9
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-08-08 22:25:22 -0700

    finally get multiple env setup working

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { ModalProvider } from 'react-modal-hook';
 import { StoreContext } from 'redux-react-hook';
 
+import { Routes } from '../routes/Routes';
 import { store } from '../store/store';
 import { GlobalStyles } from '../styles/GlobalStyles';
-import { Routes } from '../routes/Routes';
 
 const App = () => (
   <StoreContext.Provider value={store}>
