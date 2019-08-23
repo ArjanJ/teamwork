30487139e16b73ff15d9b2a760034ca181428302
Arjan Jassal <arjan.jassal.27@gmail.com>
2019-08-22 17:10:09 -0700

    REACT_APP_STAGE

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,31 +1,45 @@
 import firebase from 'firebase';
 import * as firebaseAdmin from 'firebase-admin';
 
-import * as config from './firebase-config';
+import { config } from './firebase-config';
 
-const FIREBASE_CONFIG = JSON.parse(process.env.FIREBASE_CONFIG || '');
-const adminConfig = FIREBASE_CONFIG;
+const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT;
+
+if (!GOOGLE_CLOUD_PROJECT) {
+  throw new Error(
+    'GOOGLE_CLOUD_PROJECT env variable not defined, cannot configure Firebase.',
+  );
+}
+
+const getServiceAccount = (project: string) => {
+  switch (project) {
+    case 'teamwork-dev-74882': {
+      return require('../../keys/teamwork-dev-74882-firebase-adminsdk-zbdex-3cdaaf7b04.json');
+    }
+    case 'teamwork-prod': {
+      return require('../../keys/teamwork-prod-firebase-adminsdk-abp4h-2e776c2741.json');
+    }
+    default:
+      return null;
+  }
+};
+
+const adminConfig = {
+  credential: firebaseAdmin.credential.cert(
+    getServiceAccount(GOOGLE_CLOUD_PROJECT),
+  ),
+  databaseURL: config[GOOGLE_CLOUD_PROJECT].databaseURL,
+  storageBucket: config[GOOGLE_CLOUD_PROJECT].storageBucket,
+  projectId: config[GOOGLE_CLOUD_PROJECT].projectId,
+};
+
+const clientConfig = config[GOOGLE_CLOUD_PROJECT];
 
 firebaseAdmin.initializeApp(adminConfig);
 
-const projectConfig: { [index: string]: any } = {
-  'teamwork-dev-74882': {
-    config: config.dev,
-    serviceAccount: require('../../keys/teamwork-dev-74882-firebase-adminsdk-zbdex-3cdaaf7b04.json'),
-  },
-  'teamwork-prod': {
-    config: config.prod,
-    serviceAccount: require('../../keys/teamwork-prod-firebase-adminsdk-abp4h-2e776c2741.json'),
-  },
-};
-
 if (!firebase.apps.length && adminConfig.projectId) {
-  firebase.initializeApp(projectConfig[adminConfig.projectId].config);
+  firebase.initializeApp(clientConfig);
 }
-
-adminConfig.credential = firebaseAdmin.credential.cert(
-  projectConfig[adminConfig.projectId].serviceAccount,
-);
 
 export const admin = firebaseAdmin;
 export const auth = firebase.auth();
